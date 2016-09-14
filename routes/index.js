957c1c5da98376106e9d801fb3e3dad608130e86
lakshman <diwaakartg@gmail.com>
2016-09-14 22:50:00 +0900

    checking whether line bot gives callback to heroku

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("prinitng request object");
+  console.log(req);
   res.render('index', { title: 'Express' });
 });
 
