16a894998328c6b3c0dca5aedf87df68f041f83e
shashank <ssashanks@gmail.com>
2016-09-14 23:35:26 +0900

    print req body

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,9 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   console.log("printing request object");
   console.log("Body of request is ");
-  console.log(JSON.stringify(req.body));
+  console.log(req.body);
   console.log("Request is ");
   console.log(JSON.stringify(req));
-  //var content = req.body[0].content;
-  console.log(content);
-  console.log("id"+content.id);
-  console.log("contenttype"+content.contentType);
-  console.log("text"+content.text);
   res.send(200);
 });
 
