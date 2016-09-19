92e37fecea56cf766d23b2362389205af0f296c8
shashank <ssashanks@gmail.com>
2016-09-19 22:34:14 +0900

    Call dictionary api for define keyword

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,47 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+request =  require('request'),
+router = express.Router(),
+dictionary_main = "https://api.pearson.com/v2/dictionaries/ldoce5/entries?apikey=sMtEeSSleFLGO4ijUT8ATAwHfuPEJYLg&headword=",
+error_response = "I'm afraid I don't understand. I'm sorry!",
+error_manywords = "My Bad!. I am still young. I can only define single words for now.",
+getDefinition = function(word) {
+	if(word.split(" ").length > 1) {
+		return error_manywords;
+	}
+	request(dictionary_main + word, function(error, response, body) {
+		return body;
+	});
+};
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log("printing request object");
-  console.log("Body of request is ");
-  console.log(req.body.result);
-  console.log("Request is ");
-  console.log(req.body.result[0]);
-  res.send(200);
-});
+	// Below comments to be deleted after testing first branch merge to main
+  // console.log("printing request object");
+  // console.log("Body of request is ");
+  // console.log(req.body.result);
+  // console.log("Request is ");
+  // console.log(req.body.result[0]);
 
+  var requestJson, querytext;
+  // Check if Line Request has result json object
+  if(req.body.result) {
+  	requestJson = JSON.parse(req.body.result);
+  }
+
+  // get query text typed by user
+  if(requestJson.content &&  requestJson.content.text) {
+	querytxt = requestJson.content.text;
+  }
+ // Initialize response to error
+ response = error_response;
+ if(querytxt) {
+ 	var query = querytxt.split(" ");
+ 	if(query.length > 1) {
+ 		if (query[0] === "define") {
+ 			response = getDefinition(querytxt.substring(querytxt.indexOf(" ") + 1, querytxt.length));
+ 		}
+ 	}
+ }
+ res.send(response);
+});
 module.exports = router;
