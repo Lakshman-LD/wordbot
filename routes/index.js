db6a47c3d6af7b611cc4e7b870c482320f76478e
shashank <ssashanks@gmail.com>
2016-09-20 23:39:07 +0900

    refactor code. Remove un-needed logging.

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,19 @@ var express = require('express'),
 request =  require('request'),
 router = express.Router(),
 dictionary_main = "https://api.pearson.com/v2/dictionaries/ldoce5/entries?apikey=sMtEeSSleFLGO4ijUT8ATAwHfuPEJYLg&limit=1&part_of_speech=noun%2Cadjective%2Cverb&headword=",
-string_response = "I'm afraid I don't understand. I'm sorry!",
+line_sendmessage = "https://trialbot-api.line.me/v1/events",
+string_channelid = "75ba882401bb4c4ae52123e7de5a77b1",
+string_channelsecret = "1480732716",
+string_channelmid = "u146be2a42ebbcbf77ba0e172bf54f961",
+string_noresultparam = "No result param in line's request json",
+string_errorresponse = "I'm afraid I don't understand. I'm sorry!",
 string_manywords = "My Bad!. I am still young. I can only define single words for now.",
+string_absentword = "Oops! It seems my dictionary does not have that word.",
 string_definition = "Defintion:\n",
 string_example = "\nExample:\n",
 getDefinition = function(word,replyJson, callback) {
-	console.log("y3");
 	if(word.split(" ").length > 1) {
-		return error_manywords;
+		return string_manywords;
 	}
 	console.log("Finding definition of " + word);
 	request(dictionary_main + word, function(error, response, body) {
@@ -20,22 +25,18 @@ getDefinition = function(word,replyJson, callback) {
 			console.log(JSON.stringify(replyJson));
 			sendLineResponseMessage(replyJson,callback);
 		} else {
-			//res.send(error_response);
+			res.send(string_absentword);
 		}
 	});
 }, 
 getDefintionFromDictResponse = function(body) {
-	console.log("y4");
 	var bodyJson = JSON.parse(body);
 	if(bodyJson.results && bodyJson.results.length > 0 && bodyJson.results[0].senses && bodyJson.results[0].senses.length > 0) {
-		console.log("y5");
 		var sense = bodyJson.results[0].senses[0], response;
 		if(sense.definition && sense.definition.length > 0) {
-			console.log("y6");
 			response = string_definition + sense.definition[0];
 		}
 		if(sense.examples && sense.examples.length > 0 && sense.examples[0].text) {
-			console.log("y7");
 			response +=  string_example + sense.examples[0].text;
 		}
 		return response;
@@ -45,21 +46,19 @@ sendLineResponseMessage = function(replyJson, callback) {
 	var content = {};
 	//set content here
 	request({
-	    url: "https://trialbot-api.line.me/v1/events",
+	    url: line_sendmessage,
 	    method: "POST",
 	    json: true,
 	    headers: {
 	        "content-type": "application/json",
-	        "X-Line-ChannelID": "1480732716",
-			"X-Line-ChannelSecret": "75ba882401bb4c4ae52123e7de5a77b1",
-			"X-Line-Trusted-User-With-ACL": "u146be2a42ebbcbf77ba0e172bf54f961"
+	        "X-Line-ChannelID": string_channelid,
+			"X-Line-ChannelSecret": string_channelsecret,
+			"X-Line-Trusted-User-With-ACL": string_channelmid
 	    },
 	    body: replyJson
 	}, function (error, response, body){
 		if(error) {
 			console.log("ERROR: "+error);
-		} else if(body) {
-			console.log("BODY: "+JSON.stringify(body));
 		}
 	    callback();
 	});
@@ -74,7 +73,7 @@ router.post('/line', function(req, res, next) {
   		requestJson = result[0];
   	}
   } else {
-  	console.log("No result param in line's request json");
+  	console.log(string_noresultparam);
   }
   var replyJson = {
 	  	to: [],
@@ -101,10 +100,10 @@ router.post('/line', function(req, res, next) {
 			});
  		}
  	} else {
- 		res.send(error_response);
+ 		res.send(string_errorresponse);
  	}
  } else {
- 	res.send(error_response);
+ 	res.send(string_errorresponse);
  }
 });
 module.exports = router;
